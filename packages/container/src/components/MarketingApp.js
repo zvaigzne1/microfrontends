d178ad11c2337863a043b4a35837617c7b4f075c
zvaigzne1 <63199365+zvaigzne1@users.noreply.github.com>
2021-01-21 15:24:57 +0200

    set up callback comunication line between Container Browser History and Marketing Memory History

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -1,12 +1,42 @@
-import { mount } from 'marketing/MarketingApp';
-import React, { useRef, useEffect } from 'react';
+import { mount } from "marketing/MarketingApp";
+import React, { useRef, useEffect } from "react";
+import { useHistory } from "react-router-dom";
 
 export default () => {
   const ref = useRef(null);
+  const history = useHistory();
 
   useEffect(() => {
-    mount(ref.current);
-  });
+// reseiving object from marketing/src/bootstrap.js mount function
+// and from received object we destructure onParentNavigate func
+
+    const { onParentNavigate } = mount(ref.current, {
+      onNavigate: ({ pathname: nextPathname }) => {
+        const { pathname } = history.location;
+
+        if (pathname !== nextPathname) {
+          history.push(nextPathname);
+        }
+
+        // Navigates to nextPathname
+
+        // console.log(nextPathname);
+        // console.log(location);
+        //listen function (marketing/src/bootstrap) that is calling onNavigate
+        // is going to give argument which is object "location" which
+        // gives info about where we are going navigate to inside of
+        // the marketing
+        // console.log("The container noticed navigation in Marketing");
+      },
+    });
+
+    // Anytime that there is change on browser history we call onParentNavigate
+    history.listen(onParentNavigate);
+  }, []);
+
+  // useEffect without second arg - runs allways when updated or changed marketing component
+  // with dependecy array [] as second arg run exatly one time when 
+  // marketing app component is rendered for first time
 
   return <div ref={ref} />;
 };
--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,10 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createMemoryHistory } from "history";
 import App from './App';
 
 // Mount function to start up the app
-const mount = (el) => {
-  ReactDOM.render(<App />, el);
+const mount = (el, { onNavigate }) => {
+  const history = createMemoryHistory();
+  // history object that cames back from calling createMemoryHistory has an event listener tied to it called "listen()"
+  // console.log(history);
+  // Whenever navigation "navigation === chenges in url path" occures history.listen() will call function onNavigate
+  
+  if (onNavigate) {
+  history.listen(onNavigate);
+  }
+
+
+  ReactDOM.render(<App history={history} />, el);
+
+  // Each time when container navigates it does the stuff above and the return 
+  // object below and calls onParentNavigate()
+  return {
+    // gets location object as arg because called from history object from MarketingApp
+    onParentNavigate({ pathname: nextPathname}) {
+
+      const { pathname } = history.location; // from MemoryHistory
+
+      if (pathname !== nextPathname) {
+        history.push(nextPathname);
+      }
+
+      // for not getting in infinite loop we need to check if pathname is different from nextPathname
+      // console.log(location);
+      // console.log("Container just navigated");
+    }
+  };
 };
 
 // If we are in development and in isolation,
@@ -13,7 +42,7 @@ if (process.env.NODE_ENV === 'development') {
   const devRoot = document.querySelector('#_marketing-dev-root');
 
   if (devRoot) {
-    mount(devRoot);
+    mount(devRoot, {});
   }
 }
 
